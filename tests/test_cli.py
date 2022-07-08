0c4ceb6f4fcaccd2fa6d727a27952729a4376626
John Rofrano <johnnyroy@johnrofrano.com>
2022-07-08 01:54:16 +0000

    Added test cases for create-db cli command

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from click.testing import CliRunner
+from service.utils.cli_commands import create_db
+
+class TestFlaskCLI(TestCase):
+    """Test Flask CLI Commands"""
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    def test_create_db(self):
+        """It should call the create-db command"""
+        result = self.runner.invoke(create_db)
+        self.assertEqual(result.exit_code, 0)
