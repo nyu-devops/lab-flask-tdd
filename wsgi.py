3da0cd2737c09ff68a18d3937f020bfa9ecdf4fe
John Rofrano <johnnyroy@johnrofrano.com>
2024-02-05 00:33:51 +0000

    Added PORT from env

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 """
 Web Server Gateway Interface (WSGI) entry point
 """
+
+import os
 from service import create_app
+
+PORT = int(os.getenv("PORT", "8000"))
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0", port=PORT)
