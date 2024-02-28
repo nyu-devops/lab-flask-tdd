bb0d6555cdc4e2613b5a6af80327d7343515a366
John Rofrano <johnnyroy@johnrofrano.com>
2024-02-28 16:18:16 +0000

    Changed FLASK_APP to wsgi:app

--- tests/test_cli_commands.py
+++ tests/test_cli_commands.py
@@ -1,10 +1,12 @@
 """
 CLI Command Extensions for Flask
 """
+
 import os
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
 from click.testing import CliRunner
+
 # pylint: disable=unused-import
 from wsgi import app  # noqa: F401
 from service.common.cli_commands import db_create  # noqa: E402
@@ -16,10 +18,10 @@ class TestFlaskCLI(TestCase):
     def setUp(self):
         self.runner = CliRunner()
 
-    @patch('service.common.cli_commands.db')
+    @patch("service.common.cli_commands.db")
     def test_db_create(self, db_mock):
         """It should call the db-create command"""
         db_mock.return_value = MagicMock()
-        with patch.dict(os.environ, {"FLASK_APP": "service:app"}, clear=True):
+        with patch.dict(os.environ, {"FLASK_APP": "wsgi:app"}, clear=True):
             result = self.runner.invoke(db_create)
             self.assertEqual(result.exit_code, 0)
